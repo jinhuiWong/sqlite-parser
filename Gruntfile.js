ec154b58e69d6089d8ca1bba05ff5dfeeb58f666
chengwenxin <1260077156@qq.com>
2019-12-18 15:01:48 +0800

    feat#将执行脚本中的单引号改为双引号，解决部分环境下执行报错缺陷

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
   }
   const customArgs = {
     mocha: 'test/**/*-spec.js --colors --bail --compilers=js:babel-core/register',
-    pegjs: `--trace --cache --optimize size --allowed-start-rules 'start,start_streaming' --output src/parser.js src/grammar.pegjs`
+    pegjs: `--trace --cache --optimize size --allowed-start-rules "start,start_streaming" --output src/parser.js src/grammar.pegjs`
   };
 
   const tmpDir = './.tmp/';
